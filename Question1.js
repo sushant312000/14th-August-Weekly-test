6c2e57b37e949b05ca4f059b2de148ecbdbcb420
Sushant Kumar <suskr.2000@gmail.com>
2022-08-14 22:40:09 +0530

    firstCommit

---
+++ Question1.js
@@ -0,0 +1,3 @@
+let arr = ["Red", "Green", "White", "Black"];
+console.log(arr.join(','));
+console.log(arr.join('+'));
---
+++ Question2.js
@@ -0,0 +1,2 @@
+let arr = [3, 8, 7, 6, 5, -4, 3, 2, 1];
+console.log(arr.sort());
